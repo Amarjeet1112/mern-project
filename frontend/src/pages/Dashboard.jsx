30ffb73d97ec032b7fb2f9bf4dbb6fe06e807cb3
Amarjeet Yadav <amarjeetyadav51125@gmail.com>
2026-04-22 23:13:29 +0530

    fix dashboard css error

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import "./Dashboard.css";
 import { useNavigate } from "react-router-dom";
 
 const Dashboard = () => {
